311bc08abd5bb541fb7177aede899fd4c79fe0b9
DeRoman10 <harel012345@gmail.com>
2025-11-18 20:35:12 +0200

    blocking at walls
    make the player block at walls
    player shown in border

--- HereWeGo/Screen.h
+++ HereWeGo/Screen.h
@@ -4,43 +4,31 @@
 #include "Placement.h"
 
 class Screen {
-private: //was that what u meant? 
-	int xSize, ySize;
 
-public:
-	Screen(int x_Size, int y_Size) //random ass naming for now
-	{
-		xSize = x_Size;
-		ySize = y_Size;
-	}
-
-	void draw() const;
-	void inLimit(Placement& p);
-
-	void clearScreen() { 
-		system("cls"); 
-	}
-		
 private:
+    char* screen[MAX_Y];
 
-
-	char* screen[MAX_Y + 1];
-
-	void createGameScreen();
-
-	char charAt(Placement& p) const {
-		return screen[p.gety()][p.getx()];
-	}
-
+    char charAt(Placement& p) const {
+        return screen[p.gety()][p.getx()];
+    }
 
 public:
+    Screen();
+    void createGameScreen();
 
-	bool isWall(Placement& p) const {
-		return charAt(p) == 'W';
-	}
-	bool isWonChar(Placement& p) const {
-		return charAt(p) == '%';
-	}
+    void setTile(int x, int y, char symbol) const;
+    void inLimit(Placement& p);
 
+    void clearScreen() {
+        system("cls");
+    }
 
+    bool isWall(Placement& p) const {
+        return charAt(p) == 'W';
+    }
+    bool isWonChar(Placement& p) const {
+        return charAt(p) == '%';
+    }
+
+    void draw() const;
 };
--- HereWeGo/screen.cpp
+++ HereWeGo/screen.cpp
@@ -3,6 +3,31 @@
 #include "Screen.h"
 #include "utils.h"
 
+
+void Screen::createGameScreen() {
+
+	for (int i = 0; i < MAX_Y; i++)
+	{
+		for (int j = 0; j < MAX_X; j++)
+		{
+			screen[i][j] = ' ';
+		}
+	}
+
+}
+Screen::Screen() {
+	for (int i = 0; i < MAX_Y; i++) {
+		screen[i] = new char[MAX_X + 1];
+		screen[i][MAX_X] = '\0';
+	}
+	createGameScreen();
+}
+
+
+void Screen::setTile(int x, int y, char symbol) const {
+	const_cast<char*>(screen[y])[x] = symbol;
+}
+
 void Screen::draw() const {
 	int y = 0;
 	for (const auto& row : screen) {
@@ -13,18 +38,5 @@ void Screen::draw() const {
 
 void Screen::inLimit(Placement& p)
 {
-
-}
-
-
-void Screen::createGameScreen() {
-
-	for (int i = 0; i < MAX_Y; i++)
-	{
-		for (int j = 0; j < MAX_X; j++)
-		{
-			screen[i][j] = 'W';
-		}
-	}
 
 }

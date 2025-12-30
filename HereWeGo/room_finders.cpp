a9c6c7c67786e8ef9c9eaed8b87b237980b7ad37
EitanB100 <goodsky2@gmail.com>
2025-12-30 22:03:08 +0200

    ,

--- HereWeGo/room_finders.cpp
+++ HereWeGo/room_finders.cpp
@@ -3,71 +3,51 @@
 
 // Door finders
 Door* Room::isDoorThere(const Point& p) {
-	return findBy(doors, [&](Door& d) {return d.getPos().getPosition() == p; });
+	return findBy(doors, [&](Door& door) {return door.getPos().getPosition() == p; });
 }
 
 const Door* Room::isDoorThere(const Point& p) const {
-	return findBy(doors, [&](const Door& d) {return d.getPos().getPosition() == p; });
+	return findBy(doors, [&](const Door& door) {return door.getPos().getPosition() == p; });
 }
 
 // Key finders
 Key* Room::isKeyThere(const Point& p) {
-	for (auto& key : keys) {
-		if (key.getPos() == p) return &key;
-	}
-	return nullptr;
+	return findBy(keys, [&](Key& key) {return key.getPos() == p; });
 }
 
 const Key* Room::isKeyThere(const Point& p) const {
-	for (const auto& key : keys) {
-		if (key.getPos() == p) return &key;
-	}
-	return nullptr;
+	return findBy(keys, [&](const Key& key) {return key.getPos() == p; });
 }
 
 // Switch finders
 Switch* Room::isSwitchThere(const Point& p) {
-	for (auto& sw : switches) {
-		if (sw && sw->getPos() == p) return sw.get();
-	}
-	return nullptr;
+	auto sw = findBy(switches, [&](const std::unique_ptr<Switch>& s) {return s && s->getPos() == p; });
+
+	return sw ? sw->get() : nullptr;
 }
 
 const Switch* Room::isSwitchThere(const Point& p) const {
-	for (const auto& sw : switches) {
-		if (sw && sw->getPos() == p) return sw.get();
-	}
-	return nullptr;
+	auto sw = findBy(switches, [&](const std::unique_ptr<Switch>& s) {return s && s->getPos() == p; });
+
+	return sw ? sw->get() : nullptr;
 }
 
 //Torch finders
 Torch* Room::isTorchThere(const Point& p) {
-	for (auto& torch : torches) {
-		if (torch.getPos() == p) return &torch;
-	}
-	return nullptr;
+	return findBy(torches, [&](Torch& torch) {return torch.getPos() == p; });
 }
 
 const Torch* Room::isTorchThere(const Point& p) const {
-	for (const auto& torch : torches) {
-		if (torch.getPos() == p) return &torch;
-	}
-	return nullptr;
+	return findBy(torches, [&](const Torch& torch) {return torch.getPos() == p; });
 }
 
 //Obstacle finders
 Obstacle* Room::isObstacleThere(const Point& p) {
-	for (auto& ob : obstacles) {
-		if (ob.isAt(p)) return &ob;
-	}
-	return nullptr;
+	return findBy(obstacles, [&](Obstacle& obstacle) {return obstacle.isAt(p); });
 }
 
 const Obstacle* Room::isObstacleThere(const Point& p) const {
-	for (const auto& ob : obstacles) {
-		if (ob.isAt(p)) return &ob;
-	}
-	return nullptr;
+	return findBy(obstacles, [&](const Obstacle& obstacle) {return obstacle.isAt(p); });
 }
 
 // Spring finders
